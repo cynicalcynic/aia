a14cea8cf893b019945be66c9fee7441ce864d4c
Michał Świderski <michal.sw12@gmail.com>
2020-03-25 00:09:33 +0100

    do stuff

---
+++ javascript-webscraper/src/scraper.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+
+const request = require('request-promise');
+const {JSDOM} = require('jsdom');
+const chalk = require('chalk');
+const argv = require('yargs')
+    .usage('$0 [options]')
+    .option('num', {
+        alias: 'n',
+        describe: "Display n whiskeys",
+        type: 'number'
+    })
+    .option('reverse', {
+        alias: 'r',
+        describe: 'sort ascending',
+        type: 'boolean'
+    })
+    .help('help')
+    .locale('en')
+    .argv;
+
+
+(async function () {
+    console.log('Fetching whiskeys...');
+    let productList = [];
+
+    const url = 'https://singlemalt.pl/single-malt?limit=80&mode=list';
+    let res, dom;
+    try {
+        res = await request.get(url);
+        dom = new JSDOM(res);
+    } catch (e) {
+        console.error(chalk.bold.red('Connection failed\nQuitting...'));
+        process.exit(1);
+    }
+
+    let page = 1;
+    while (true) {
+        if (dom.window.document.querySelectorAll('.next').length === 0) {
+            break;
+        }
+
+        try {
+            res = await request.get(
+                `${url}&p=${page++}`
+            );
+            dom = new JSDOM(res);
+        } catch (e) {
+            console.error(e.message + '\nQuitting...');
+            process.exit(1);
+        }
+
+        const list = dom.window.document.querySelectorAll('#products-list li');
+        if (list.length === 0) {
+            console.error(chalk.bold.red('Something went wrong...\nQuitting...'));
+            process.exit(1);
+        }
+        list.forEach((el) => {
+
+            let productName = el.querySelector('.product-name>a').innerHTML;
+            const match = productName.match(/(?<age>[0-9]+) (Years? Old)/);
+            if (match) {
+                let price = el.querySelector('.price').innerHTML;
+                price = Number.parseInt(price.replace('&nbsp;', '').replace(',', '.').split('zł')[0]);
+                const age = Number.parseInt(match.groups.age);
+                const volume = Number.parseFloat(productName.match(/(?<volume>[0-9](,[0-9]+)?) l/).groups.volume.replace(',', '.'));
+                const unitPrice = Number.parseFloat((price / (volume * 10) / age).toFixed(2));
+                productList.push({
+                    name: productName,
+                    unitPrice
+                });
+            }
+        });
+    }
+
+    productList.sort((a, b) => {
+        return argv.reverse ? a.unitPrice - b.unitPrice : b.unitPrice - a.unitPrice;
+    });
+
+    if (argv.num && argv.num > 0) {
+        productList = productList.slice(0, argv.num);
+    }
+
+    console.log(`Fetched ${chalk.bold.yellow(productList.length)} whiskeys`);
+    console.log(chalk.bold('No'.padEnd(4) + `Name`.padEnd(128) + 'Unit price [zł/100ml/year]'));
+    productList.forEach((product, index) => {
+        console.log((index + 1).toString().padEnd(3), product.name.padEnd(127), product.unitPrice.toString().padEnd(4, '0'));
+    });
+})();
+
+
+
