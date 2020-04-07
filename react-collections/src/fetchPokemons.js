22e4f7b17fe2f90e298c07fda4350e16b00078d7
Michał Świderski <michal.sw12@gmail.com>
2020-04-08 00:59:43 +0200

    add pokemon list and pokemon fetching script

---
+++ react-collections/src/fetchPokemons.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const fs = require('fs');
+
+(async function () {
+    const pokemons = [];
+    const {data} = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20");
+
+    const requests = data.results.map(pokemon => {
+        let p = {};
+        return axios.get(pokemon.url).then(({data}) => {
+            p = {
+                id: data.id,
+                name: data.name,
+                image: data.sprites.front_default,
+                rating: Math.floor(Math.random() * (5 - 1 + 1)) + 1
+            };
+            return data.species.url
+        }).then(url => {
+            return axios.get(url).then(({data}) => p.description = data.flavor_text_entries.find(entry => entry.language.name === 'en').flavor_text);
+        }).then(() => pokemons.push(p));
+    });
+
+    await Promise.all(requests);
+    pokemons.sort((a, b) => a.id - b.id);
+    fs.writeFileSync('pokemons.json', JSON.stringify(pokemons));
+    // console.log(pokemons);
+})();
