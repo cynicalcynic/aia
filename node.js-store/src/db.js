ed86ed8f5bd188cfd30bf43802657fdc0d3ff35b
Michał Świderski <swiderskimichal@outlook.com>
2020-05-05 12:50:50 +0200

    remove redundant code

--- node.js-store/src/db.js
+++ node.js-store/src/db.js
@@ -1,22 +1,3 @@
-// const sqlite3 = require('sqlite3').verbose();
-//
-// let connection = null;
-//
-// function getConnection() {
-//     if (connection) return connection;
-//
-//     connection = new sqlite3.Database('data.db', (err) => {
-//         if (err) {
-//             throw(err);
-//         }
-//     });
-//     return connection;
-// }
-//
-// module.exports = {
-//     getConnection
-// }
-
 const Database = require('better-sqlite3');
 const db = new Database('data.db');
 
